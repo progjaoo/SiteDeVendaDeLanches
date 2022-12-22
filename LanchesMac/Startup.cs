75ed1d602641303fd22eb1152dad305ae1c219d5
Joao marcos valente <joaomarcoslars.81@gmail.com>
2022-12-22 15:10:56 -0300

    up

---
+++ LanchesMac/Startup.cs
@@ -0,0 +1,35 @@
+﻿using LanchesMac.Areas.Admin.Servicos;
+using LanchesMac.Context;
+using LanchesMac.Models;
+using LanchesMac.Repositories;
+using LanchesMac.Repositories.Interfaces;
+using LanchesMac.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using ReflectionIT.Mvc.Paging;
+
+namespace LanchesMac;
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        
+
+
+
+
+    }
+
+    public void Configure(IApplicationBuilder app,
+        IWebHostEnvironment env, ISeedUserRoleInitial seedUserRoleInitial)
+    {
+        
+    }
+}
